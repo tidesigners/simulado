d3b04d82ce738dedbe5009e1ffd5b8d5c9fc65cb
Rodrigo Sardinha <rodrigo.santos@me.com>
2017-02-11 00:07:02 -0200

    Estrutura  inicial (ajustes)

---
+++ src/main/java/br/com/tiupgrade/simulado/repositorio/TodasAsMaterias.java
@@ -0,0 +1,13 @@
+package br.com.tiupgrade.simulado.repositorio;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ * Created by rodrigosantos on 10/02/17.
+ */
+
+@Named("todasAsMaterias")
+@RequestScoped
+public class TodasAsMaterias {
+}
